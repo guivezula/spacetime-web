d17b9d50030407f43ff019eeec615cc736b7222c
Guilherme Vezula <guilherme.vezula@pluritech.com.br>
2023-05-21 15:54:35 -0300

    add homepage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,31 @@
+import { ReactNode } from 'react'
 import './globals.css'
-import { Roboto_Flex as Roboto, Bai_Jamjuree as BaiJamjuree } from 'next/font/google'
+import {
+  Roboto_Flex as Roboto,
+  Bai_Jamjuree as BaiJamjuree,
+} from 'next/font/google'
 
 const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
-const baiJamjuree = BaiJamjuree({ subsets: ['latin'], weight: '700', variable: '--font-bai-jamjuree' })
+const baiJamjuree = BaiJamjuree({
+  subsets: ['latin'],
+  weight: '700',
+  variable: '--font-bai-jamjuree',
+})
 
 export const metadata = {
   title: 'NLW Space Time',
-  description: 'Uma cápsula do tempo construída com React, Next.js, TailwindCSS e TypeScript.',
+  description:
+    'Uma cápsula do tempo construída com React, Next.js, TailwindCSS e TypeScript.',
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className={`${roboto.variable} ${baiJamjuree.variable} font-sans`}>{children}</body>
+      <body
+        className={`${roboto.variable} ${baiJamjuree.variable} bg-gray-900 font-sans text-gray-100`}
+      >
+        {children}
+      </body>
     </html>
   )
 }
