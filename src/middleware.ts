8fd58a5c37ad1a787409baedfcb18db34e3153b6
Guilherme Vezula <guilherme.vezula@pluritech.com.br>
2023-05-21 23:18:23 -0300

    add moddleware

---
+++ src/middleware.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+export function middleware(request: NextRequest) {
+  const token = request.cookies.get('token')?.value
+
+  const signInUrl = `https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}`
+
+  if (!token) {
+    return NextResponse.redirect(signInUrl, {
+      headers: {
+        'Set-Cookie': `redirectTo=${request.url}; Path=/; HttpOnly; max-age=20`,
+      },
+    })
+  }
+
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: '/memories/:path*',
+}
