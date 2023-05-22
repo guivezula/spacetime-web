8cf47c8ed88ec4016df37a26c094bc5da55d129d
Guilherme Vezula <guilherme.vezula@pluritech.com.br>
2023-05-21 22:58:00 -0300

    create new memory page

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -16,7 +16,7 @@ export function Hero() {
       </div>
 
       <a
-        href=""
+        href="/memories/new"
         className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600"
       >
         CADASTRAR LEMBRANÇA
