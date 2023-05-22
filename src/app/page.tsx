5ec373c24d73995aef9951c75575261ceb9980c1
Guilherme Vezula <guilherme.vezula@pluritech.com.br>
2023-05-21 21:47:30 -0300

    add auth profile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import { cookies } from 'next/headers'
 import { Copyright } from '@/components/Copyright'
 import { EmptyMemories } from '@/components/EmptyMemories'
 import { Hero } from '@/components/Hero'
 import { Signin } from '@/components/Signin'
+import { Profile } from '@/components/Profile'
 
 export default function Home() {
+  const isAuthenticated = cookies().has('token')
+
   return (
     <main className="grid min-h-screen grid-cols-2">
       <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
@@ -13,7 +17,7 @@ export default function Home() {
         {/* Stripes */}
         <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
 
-        <Signin />
+        {isAuthenticated ? <Profile /> : <Signin />}
         <Hero />
         <Copyright />
       </div>
